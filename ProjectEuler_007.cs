5d97a73ee995401d82efccde2cb5e5c41adf516e
JT Stukes <jt.stukes@live.com>
2023-10-23 13:11:43 -0400

    Added Problems 007 and 014 solutions.
    I had previously forgotten problem 007. They are being coded/completed in order.

---
+++ ProjectEuler_007.cs
@@ -0,0 +1,51 @@
+﻿// Project Euler (https://projecteuler.net/) code exercises
+// Taken from FreeCodeCamp (https://www.freecodecamp.org/learn/project-euler/) lesson plan
+// Not coded in JavaScript, but C#
+// Code by JT Stukes
+
+// Problem 7 - 10001st prime
+
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the nth prime number?
+*/
+
+Console.WriteLine(nthPrime(6)); // should write 13
+Console.WriteLine(nthPrime(10)); // should write 29
+Console.WriteLine(nthPrime(100)); // should write 541
+Console.WriteLine(nthPrime(1000)); // should write 7919
+Console.WriteLine(nthPrime(10001)); // should write 104743
+
+long nthPrime(int goalPrime)
+{
+    int totalPrimesFound = 1; // two already counted
+    long currentPrime = 2;
+    long nextNumberToCheck = 3;
+    while (totalPrimesFound < goalPrime)
+    {
+        if (IsPrime(nextNumberToCheck))
+        {
+            currentPrime = nextNumberToCheck;
+            totalPrimesFound++;
+        }
+        nextNumberToCheck += 2;
+    }
+    return currentPrime;
+}
+
+// IsPrime() Copied from Problem 3
+bool IsPrime(long number)
+{
+    long nextFactor = 2;
+
+    while (nextFactor * nextFactor <= number)
+    {
+        if (number % nextFactor == 0)
+        {
+            return false;
+        }
+        nextFactor++;
+    }
+    return true;
+}
---
+++ ProjectEuller_014.cs
@@ -0,0 +1,65 @@
+﻿// Project Euler (https://projecteuler.net/) code exercises
+// Taken from FreeCodeCamp (https://www.freecodecamp.org/learn/project-euler/) lesson plan
+// Not coded in JavaScript, but C#
+// Code by JT Stukes
+
+// Problem 14 - Longest Colatz Sequence
+
+/*
+The following iterative sequence is defined for the set of positive integers:
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under the given limit, produces the longest chain?
+
+Note: Once the chain starts the terms are allowed to go above limit.
+*/
+
+//
+
+Console.WriteLine(LongestCollatzSequence(14)); // should write 9
+Console.WriteLine(LongestCollatzSequence(5847)); // should write 3711
+Console.WriteLine(LongestCollatzSequence(46500)); // should write 35655
+Console.WriteLine(LongestCollatzSequence(54512)); // should write 52527
+Console.WriteLine(LongestCollatzSequence(100000)); // should write 77031
+Console.WriteLine(LongestCollatzSequence(1000000)); // should write 837799
+
+int LongestCollatzSequence(int startingNumber)
+{
+    // creating an array wich each position representing the starting number
+    int[] allCollatz = new int[startingNumber + 1];
+
+    for (int i = 1; i <= startingNumber; i++)
+    {
+        int currentCount = 0;
+        long collatzValue = i;
+        while (collatzValue != 1)
+        {
+            currentCount++;
+            if (collatzValue % 2 ==0)
+            {
+                collatzValue = collatzValue / 2;
+            }
+            else
+            {
+                collatzValue = (3 * collatzValue) + 1;
+            }
+            if (collatzValue < i) // check for existing Collatz calculation and add steps
+            {
+                currentCount += allCollatz[collatzValue];
+                collatzValue = 1;
+            }
+            //Console.Write($" {collatzValue}");
+        }
+       // Console.WriteLine($" count = {currentCount}");
+        allCollatz[i] = currentCount;
+    }
+
+    int maxSteps = allCollatz.Max();
+    return Array.IndexOf(allCollatz, maxSteps);
+}
