fe84905132c6dd6b75307ea7adfb8337d0fbe33e
JT Stukes <jt.stukes@live.com>
2023-10-24 21:42:49 -0400

    Completed problems 17 and 18
    18 solved in such a manner that is should also solve 67

---
+++ ProjectEuler_017.cs
@@ -0,0 +1,212 @@
+﻿// Project Euler (https://projecteuler.net/) code exercises
+// Taken from FreeCodeCamp (https://www.freecodecamp.org/learn/project-euler/) lesson plan
+// Not coded in JavaScript, but C#
+// Code by JT Stukes
+
+// Problem 17 - Number Letter Counts
+
+/*
+If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+NOTE: Do not count spaces or hyphens. For example, 346 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+*/
+
+Console.WriteLine(NumberLetterCounts(5)); // should write 19
+Console.WriteLine(NumberLetterCounts(150)); // should write 1903
+Console.WriteLine(NumberLetterCounts(1000)); // should write 21124
+
+
+string andString = "";
+
+int NumberLetterCounts(int countTo)
+{
+    string wordSmash = "";
+
+    for (int i = 1; i <= countTo; i++)
+    {
+        if (i < 1000)
+        {
+            andString = "and";
+        }
+        else
+        {
+            andString = "";
+        }
+
+        switch (i)
+        {
+            // ones and tens (1-99)
+            case <= 99:
+                wordSmash = wordSmash + WriteTens(i);
+                break;
+
+            // hundreds (100 - 999)
+            case <= 999:
+                wordSmash = wordSmash + WriteHundreds(i);
+                break;
+
+            // thousands (1000 - 999,999)
+            case <= 999999:
+                wordSmash = wordSmash + WriteThousands(i);
+                break;
+
+            // millions (1,000,000 - 999,999,999)
+            case <= 999999999:
+                //wordSmash = wordSmash + WriteMillions(i);
+                break;
+
+            default:
+                Console.WriteLine("Failure: Default in NumberLetterCounts()");
+                //wordSmash = "";
+                break;
+        }
+    }
+    //Console.WriteLine(wordSmash);
+    return wordSmash.Length;
+}
+
+string WriteOnes(int number)
+{
+    switch (number)
+    {
+        case 0:
+            return "";
+            break;
+        case 1:
+            return "one";
+            break;
+        case 2:
+            return "two";
+            break;
+        case 3:
+            return "three";
+            break;
+        case 4:
+            return "four";
+            break;
+        case 5:
+            return "five";
+            break;
+        case 6:
+            return "six";
+            break;
+        case 7:
+            return "seven";
+            break;
+        case 8:
+            return "eight";
+            break;
+        case 9:
+            return "nine";
+            break;
+        default:
+            Console.WriteLine("Failure: Default in WriteOnes()");
+            return "";
+            break;
+    }
+}
+
+string WriteTens(int number)
+{
+    switch (number)
+    {
+        case <= 9:
+            return WriteOnes(number);
+            break;
+        case 10:
+            return "ten";
+            break;
+        case 11:
+            return "eleven";
+            break;
+        case 12:
+            return "twelve";
+            break;
+        case 13:
+            return "thirteen";
+            break;
+        case 14:
+            return "fourteen";
+            break;
+        case 15:
+            return "fifteen";
+            break;
+        case 16:
+            return "sixteen";
+            break;
+        case 17:
+            return "seventeen";
+            break;
+        case 18:
+            return "eighteen";
+            break;
+        case 19:
+            return "nineteen";
+            break;
+        case <= 29:
+            return "twenty" + WriteOnes(number % 10);
+            break;
+        case <= 39:
+            return "thirty" + WriteOnes(number % 10);
+            break;
+        case <= 49:
+            return "forty" + WriteOnes(number % 10);
+            break;
+        case <= 59:
+            return "fifty" + WriteOnes(number % 10);
+            break;
+        case <= 69:
+            return "sixty" + WriteOnes(number % 10);
+            break;
+        case <= 79:
+            return "seventy" + WriteOnes(number % 10);
+            break;
+        case <= 89:
+            return "eighty" + WriteOnes(number % 10);
+            break;
+        case <= 99:
+            return "ninety" + WriteOnes(number % 10);
+            break;
+        default:
+            Console.WriteLine("Failure: Default in WriteTens()");
+            return "";
+            break;
+    }
+}
+
+string WriteHundreds(int number)
+{
+    if (number / 100 < 1)
+    {
+        return WriteTens(number);
+    }
+
+    if (number % 100 == 0)
+    {
+        return WriteOnes(number / 100) + "hundred";
+    }
+    else
+    {
+        return WriteOnes(number / 100) + "hundred" + andString + WriteTens(number % 100);
+
+    }
+}
+
+string WriteThousands(int number)
+{
+    if (number / 1000 < 1)
+    {
+        Console.WriteLine("Failue: Default in WriteThousands()");
+        return "";
+    }
+    else if (number % 1000 == 0)
+    {
+        return WriteHundreds(number / 1000) + "thousand";
+    }
+    else
+    {
+        return WriteHundreds(number / 1000) + "thousand" + WriteHundreds(number % 1000);
+    }
+}
---
+++ ProjectEuler_018.cs
@@ -0,0 +1,93 @@
+﻿// Project Euler (https://projecteuler.net/) code exercises
+// Taken from FreeCodeCamp (https://www.freecodecamp.org/learn/project-euler/) lesson plan
+// Not coded in JavaScript, but C#
+// Code by JT Stukes
+
+// Problem 18 - Maximum Path Sum I
+
+/*
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+*/
+
+int[,] triangle =   {   {75,00,00,00,00,00,00,00,00,00,00,00,00,00,00},
+                        {95,64,00,00,00,00,00,00,00,00,00,00,00,00,00},
+                        {17,47,82,00,00,00,00,00,00,00,00,00,00,00,00},
+                        {18,35,87,10,00,00,00,00,00,00,00,00,00,00,00},
+                        {20,04,82,47,65,00,00,00,00,00,00,00,00,00,00},
+                        {19,01,23,75,03,34,00,00,00,00,00,00,00,00,00},
+                        {88,02,77,73,07,63,67,00,00,00,00,00,00,00,00},
+                        {99,65,04,28,06,16,70,92,00,00,00,00,00,00,00},
+                        {41,41,26,56,83,40,80,70,33,00,00,00,00,00,00},
+                        {41,48,72,33,47,32,37,16,94,29,00,00,00,00,00},
+                        {53,71,44,65,25,43,91,52,97,51,14,00,00,00,00},
+                        {70,11,33,28,77,73,17,78,39,68,17,57,00,00,00},
+                        {91,71,52,38,17,14,91,43,58,50,27,29,48,00,00},
+                        {63,66,04,68,89,53,67,30,73,16,69,87,40,31,00},
+                        {04,62,98,27,23,09,70,98,73,93,38,53,60,04,23}
+                    };
+
+int[,] triangle2 =  {   {3,0,0,0},
+                        {7,4,0,0},
+                        {2,4,6,0},
+                        {8,5,9,3}
+                    };
+
+Console.WriteLine(MaxPathSumI(triangle)); // should write 1074
+Console.WriteLine(MaxPathSumI(triangle2)); // sholud write 23
+
+
+// copied GridDimensionsX/Y from Problem 11
+int GridDimensionY(int[,] grid) // requires non-jagged grid
+{
+    return grid.GetLength(0);
+}
+
+int GridDimensionX(int[,] grid) // requires non-jagged grid
+{
+    return grid.Length / grid.GetLength(0);
+}
+
+int MaxPathSumI(int[,] triangle)
+{
+    int triangleColumns = GridDimensionX(triangle);
+    int triangleRows = GridDimensionY(triangle);
+
+    int[,] triangleCopy = triangle;
+
+    for (int row = triangleRows - 1; row > 0; row--)
+    {
+        for (int column = 0; column < row; column++)
+        {
+            int adder = Math.Max(triangleCopy[row,column],triangleCopy[row,column+1]);
+            triangleCopy[row - 1,column] = triangleCopy[row - 1,column] + adder;
+        }
+    }
+
+    return triangleCopy[0,0];
+}
